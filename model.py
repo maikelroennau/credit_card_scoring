b8317b6fe3237a59dd02bb0754b3a6320cd0fdd4
Maike Maciel Rönnau <ronnau@hp.com>
2018-05-13 22:35:50 -0300

    Added save, load and predict functions.

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from sklearn.linear_model import LogisticRegression
 from numpy.random import RandomState
 from sklearn.metrics import roc_auc_score
 
+from constants import *
+
 import os
 import pickle
 from datetime import datetime
 
-
-trained_models_path = "trained_models/"
 
 def split_dataset(df, validation_percentage, seed):
     state = RandomState(seed)
@@ -16,6 +16,16 @@ def split_dataset(df, validation_percentage, seed):
     training_set = df.loc[~df.index.isin(validation_indexes)]
     validation_set = df.loc[df.index.isin(validation_indexes)]
     return training_set, validation_set
+
+
+def save_model(model_data, path=MODELS_PATH):
+    # Saving the model with the current date and hour
+    filename = "model_{}.sav".format(datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
+    pickle.dump(model_data, open(os.path.join(path, filename), "wb"))
+
+
+def load_model(model_name=CURRENT_MODEL, path=MODELS_PATH):
+    return pickle.load(open(os.path.join(path, model_name), 'rb'))
 
 
 def train(data_path):
@@ -39,27 +49,16 @@ def train(data_path):
     validation_predictions = clf.predict_proba(validation_set[["score_3", "score_4", "score_5", "score_6"]])[:, 1]
     print(roc_auc_score(validation_set[["default"]], validation_predictions))
 
-    filename = "model_{}.sav".format(datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
-    pickle.dump(clf, open(os.path.join(trained_models_path, filename), "wb"))
+    # Saving model to disk, so we can use it later
+    save_model(clf)
 
 
-def load_model(model_path, data_path):
-    model = pickle.load(open(os.path.join(trained_models_path, model_path), 'rb'))
+def predict(model, subject):
+    # model: loaded model
+    # subject: dict with data to be predicted by the model 
+    df = pd.DataFrame([subject], columns=subject.keys())
+    return model.predict_proba(df[["score_3", "score_4", "score_5", "score_6"]])[:, 1][0]
 
-    data = pd.read_parquet(data_path)
-
-    # # split into training and validation
-    # training_set, validation_set = split_dataset(data, 0.25, 1)
-    # print("training set has %s rows" % len(training_set))
-    # print("validation set has %s rows" % len(validation_set))
-
-    # validation_set["score_3"] = validation_set["score_3"].fillna(455)
-    # validation_set["default"] = validation_set["default"].fillna(False)
-    # validation_predictions = model.predict_proba(validation_set[["score_3", "score_4", "score_5", "score_6"]])[:, 1]
-    # print(roc_auc_score(validation_set[["default"]], validation_predictions))
 
 if __name__ == "__main__":
     pass
-    # train("training_set.parquet")
-    # load_model("model_2018-05-13_15h11m19s.sav", "training_set.parquet")
-
