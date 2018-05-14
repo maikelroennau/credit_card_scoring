b57f3643af6890ade8383694af40ad1ad956be6c
Maike Maciel Rönnau <ronnau@hp.com>
2018-05-13 22:35:18 -0300

    Implementation with base for service execution.

--- http_server.py
+++ http_server.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 """
-Very simple HTTP server in python.
-Usage::
-    ./dummy-web-server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
 Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
+    curl -d '{"id": "8db4206f-8878-174d-7a23-dd2c4f4ef5a0", "score_3": 480.0, "score_4": 105.2, "score_5": 0.8514, "score_6": 94.2, "income": 500000}' localhost:8080
+    Using port '8080' so it won't conflict with any other service
 """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
+from model import predict, load_model
+from constants import *
+import json
+
 
 class CreditCardScoring(BaseHTTPRequestHandler):
+    
     def _set_headers(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/json")
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
 
 
@@ -27,24 +26,34 @@ class CreditCardScoring(BaseHTTPRequestHandler):
         # Send infrastructure information
 
 
-    def do_POST(self):
-        content_length = int(self.headers["Content-Length"])
-        post_data = self.rfile.read(content_length)
-
-        # Validate input
-        # Run accordingly
-        #   Predict
-        #   Train model
-        #   Update model
-
+    def do_HEAD(self):
         self._set_headers()
-        self.wfile.write('{"id": "8db4206f-8878-174d-7a23-dd2c4f4ef5a0", "prediction": 0.1495 }')
+
+
+    def do_POST(self):
+        self._set_headers()
+        # Read input
+        self.data_input = self.rfile.read(int(self.headers['Content-Length']))
+        self.send_response(200)
+        self.end_headers()
+
+        # Converting data to JSON and getting a prediction
+        data = json.loads(self.data_input)
+        prediction = predict(load_model(), data)
+
+        # Building JSON response
+        response = {}
+        response["id"] = data["id"]
+        response["prediction"] = prediction
+        json_response = json.dumps(response)
+
+        self.wfile.write(json.dumps(response))
 
 
 def run(server_class=HTTPServer, handler_class=CreditCardScoring, port=8080):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
-    print("Starting server...")
+    print("Server running on port {}".format(port))
     httpd.serve_forever()
 
 
--- model.py
+++ model.py
@@ -3,6 +3,12 @@ from sklearn.linear_model import LogisticRegression
 from numpy.random import RandomState
 from sklearn.metrics import roc_auc_score
 
+import os
+import pickle
+from datetime import datetime
+
+
+trained_models_path = "trained_models/"
 
 def split_dataset(df, validation_percentage, seed):
     state = RandomState(seed)
@@ -12,14 +18,14 @@ def split_dataset(df, validation_percentage, seed):
     return training_set, validation_set
 
 
-def train():
+def train(data_path):
     # load the data
-    data = pd.read_parquet("training_set.parquet")
+    data = pd.read_parquet(data_path)
 
     # split into training and validation
     training_set, validation_set = split_dataset(data, 0.25, 1)
-    print('training set has %s rows' % len(training_set))
-    print('validation set has %s rows' % len(validation_set))
+    print("training set has %s rows" % len(training_set))
+    print("validation set has %s rows" % len(validation_set))
 
     # train model
     training_set["score_3"] = training_set["score_3"].fillna(425)
@@ -33,6 +39,27 @@ def train():
     validation_predictions = clf.predict_proba(validation_set[["score_3", "score_4", "score_5", "score_6"]])[:, 1]
     print(roc_auc_score(validation_set[["default"]], validation_predictions))
 
+    filename = "model_{}.sav".format(datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
+    pickle.dump(clf, open(os.path.join(trained_models_path, filename), "wb"))
 
-if __name__ == '__main__':
-    train()
+
+def load_model(model_path, data_path):
+    model = pickle.load(open(os.path.join(trained_models_path, model_path), 'rb'))
+
+    data = pd.read_parquet(data_path)
+
+    # # split into training and validation
+    # training_set, validation_set = split_dataset(data, 0.25, 1)
+    # print("training set has %s rows" % len(training_set))
+    # print("validation set has %s rows" % len(validation_set))
+
+    # validation_set["score_3"] = validation_set["score_3"].fillna(455)
+    # validation_set["default"] = validation_set["default"].fillna(False)
+    # validation_predictions = model.predict_proba(validation_set[["score_3", "score_4", "score_5", "score_6"]])[:, 1]
+    # print(roc_auc_score(validation_set[["default"]], validation_predictions))
+
+if __name__ == "__main__":
+    pass
+    # train("training_set.parquet")
+    # load_model("model_2018-05-13_15h11m19s.sav", "training_set.parquet")
+
