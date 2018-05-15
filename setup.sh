503a76d56a51d9f3eb7c3cd33aef42fc3d48089e
Maike Maciel Rönnau <ronnau@hp.com>
2018-05-14 23:11:20 -0300

    Update setup script. We can go with this one, bu it can be better...

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-DIRECTORY="trained_models"
+DIRECTORY=trained_models
 PORT=8080
 
 if [ ! -d "$DIRECTORY" ]; then
   mkdir $DIRECTORY
 fi
 
-if [ "$(ls -A $DIRECTORY)" ]; then
+if [ ! "$(ls -A $DIRECTORY)" ]; then
     echo "No model found."
     echo "Training model..."
 
     python model.py train
+    echo "Finish training model."
 fi
 
 echo "Starting server" 
