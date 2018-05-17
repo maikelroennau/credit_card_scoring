e5f200733ec51513a822394058c4884256111dfa
Maikel Maciel Rönnau <ronnau@hp.com>
2018-05-16 21:15:48 -0700

    Added new template of tests.

--- run_tests.py
+++ run_tests.py
@@ -41,11 +41,33 @@ def start_server():
 def development_test():
     data = {"id": "8db4206f-8878-174d-7a23-dd2c4f4ef5a0", "score_3": 480.0, "score_4": 105.2, "score_5": 0.8514, "score_6": 94.2, "income": 500000}
 
-    r = requests.post("http://localhost:8091/predict", json=data)
+    r = requests.post("http://localhost:8080/predict", json=data)
 
+    print r.text
+
+
+def development_test_2():
+    data = {"dataset_path": "training_set_2.parquet"}
+
+    r = requests.post("http://localhost:8080/train", json=data)
+
+    print r.text
+
+
+def development_test_3():
+    r = requests.get("http://localhost:8080/config")
+    print r.text
+
+
+def development_test_4():
+    data = {"model_name" : "model_2018-05-16_21h10m21s.sav"}
+    r = requests.post("http://localhost:8080/update_model", json=data)
     print r.text
 
 
 if __name__ == "__main__":
     # test_predict()
-    development_test()
+    # development_test()
+    # development_test_2()
+    # development_test_3()
+    development_test_4()
