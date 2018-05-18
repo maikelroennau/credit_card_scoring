d57bd851408a17850496ba6915eaa5cb116aa361
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2018-05-17 21:14:57 -0700

    Small corrections and updates.

--- server_run.py
+++ server_run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import json
+import os
+
 from flask import Flask, request
 
 from model import load_model, predict, train
@@ -9,24 +11,27 @@ from model import load_model, predict, train
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def get_prediction():
     input_json = request.json
 
     # Getting a prediction for inputed data
-    prediction = predict(curent_model, input_json)
+    prediction = predict(current_model, input_json)
 
     # Building JSON response
     response = {}
     response["id"] = input_json["id"]
     response["prediction"] = prediction
 
-    return str(response)
+    return json.dumps(response)
 
 
-@app.route('/train', methods=['POST'])
+@app.route("/train", methods=["POST"])
 def train_model():
     input_json = request.json
+
+    if not os.path.exists(input_json["dataset_path"]):
+        return str({})
 
     model_name, score = train(input_json["dataset_path"])
 
@@ -35,43 +40,50 @@ def train_model():
     response["model_name"] = model_name
     response["score"] = score
 
-    return str(response)
+    return json.dumps(response)
 
 
-@app.route('/update_model', methods=['POST'])
+@app.route("/update_model", methods=["POST"])
 def update_model():
-    import os
+    global current_model
 
     input_json = request.json
 
-    curent_model = load_model(input_json["model_name"])
+    current_model = load_model(model_name=input_json["model_name"])
 
     # Loading config file to be updated with new model
-    json_data = open(config_file).read()
-    config = json.loads(json_data)
-    config["current_model"] = os.path.join(config["model_dir"], input_json["model_name"])
+    config = load_config(config_file)
+    config["current_model"] = input_json["model_name"]
 
-    with open(config_file, 'w') as outfile:
+    with open(config_file, "w") as outfile:
         json.dump(config, outfile)
 
     # Building JSON response
     response = {}
     response["current_model"] = config["current_model"]
 
-    return str(response)
+    return json.dumps(response)
 
 
-@app.route('/config', methods=['GET'])
+@app.route("/config", methods=["GET"])
 def get_config():
-    # Loadin config file and sending it back
-    json_data = open(config_file).read()
-    config = json.loads(json_data)
-
-    return str(config)
+    return json.dumps(load_config(config_file))
 
 
-if __name__ == '__main__':
+def load_config(config_path):
+    # Loading config file from disk
+    with open(config_path, "r") as json_data:
+        config = json.loads(json_data.read())
+        return config
+
+
+if __name__ == "__main__":
     config_file = "config.json"
-    curent_model = load_model()
+    current_model = load_model()
 
-    app.run(port='8080')
+    if current_model is None:
+        print("No model was found.")
+        print("Stopping execution")
+        exit(1)
+
+    app.run(port=8080)
